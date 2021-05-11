edafb173ff0ef96223c4154e3b46af34ccd337bb
mohanson <mohanson@outlook.com>
2021-05-11 11:28:54 +0800

    Fix checkout.sh

--- tools/checkout.sh
+++ tools/checkout.sh
@@ -7,6 +7,7 @@ if [ ! -d riscv-gcc ]; then
 	git clone git@github.com:riscv/riscv-gcc.git riscv-gcc-new
 	cd riscv-gcc-new
 	git checkout riscv-bitmanip
+	git checkout 3a004f3c3bd941d79adba5e7c0bc643f745027be
 	# rebase occasionally to upstream/trunk
 	git remote add upstream git://gcc.gnu.org/git/gcc.git
 	git fetch upstream
@@ -19,6 +20,7 @@ if [ ! -d riscv-binutils ]; then
 	git clone git@github.com:riscv/riscv-binutils-gdb.git riscv-binutils-new
 	cd riscv-binutils-new
 	git checkout riscv-bitmanip
+	git checkout c870418800cd390bb2ae531226efd8a8ce1b741d
 	# rebase occasionally to upstream/master
 	git remote add upstream git://sourceware.org/git/binutils-gdb.git
 	git fetch upstream
@@ -28,9 +30,9 @@ fi
 
 if [ ! -d riscv-isa-sim ]; then
 	rm -rf riscv-isa-sim-new
-	git clone git@github.com:riscv/riscv-isa-sim.git riscv-isa-sim-new
+	git clone git@github.com:libraries/riscv-isa-sim.git riscv-isa-sim-new
 	cd riscv-isa-sim-new
-	# git checkout riscv-bitmanip
+	git checkout riscv-bitmanip
 	cd ..
 	mv riscv-isa-sim-new riscv-isa-sim
 fi
